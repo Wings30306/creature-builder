6afdbeffe10c28300878015dd2bba83612e38733
Joke Heyndels <jo_hannah@outlook.com>
2020-09-15 10:49:21 +0200

    Full code from assignment + embellishments to ask for user input

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreatureBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
